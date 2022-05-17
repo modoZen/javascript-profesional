11988bc964d983e0dcfffd907777c0afeed8a830
Max <mdiazn@uni.pe>
2022-05-17 17:37:05 -0500

    clase7

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,5 +1,14 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins ||  [];
+
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    });
 }
 
 MediaPlayer.prototype.play = function () {
@@ -13,6 +22,15 @@ MediaPlayer.prototype.pause = function() {
 MediaPlayer.prototype.togglePlay = function () {
     this.media.paused?this.play():this.pause();
 }
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.toggleMute = function () {
+    this.media.muted = !this.media.muted;
+}
+
 
 // class MediaPlayer{
 //   constructor(config){
