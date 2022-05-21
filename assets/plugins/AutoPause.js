6c4870b693e27dd6906b91eb9c529719c8ae0808
Max <mdiazn@uni.pe>
2022-05-21 17:27:00 -0500

    clase21_1

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,25 +1,43 @@
 class AutoPause{
     constructor(){
-        
+        this.handlerIntersection = this.handlerIntersection.bind(this);
     }
-
     run(player){
         this.player = player;
-        const observer = new IntersectionObserver(this.handlerIntersection(), {
+        const observer = new IntersectionObserver(this.handlerIntersection, {
             threshold: 0.25
         })
 
         observer.observe(player.media);
     }
 
-    handlerIntersection(){
-        return (entries)=>{
-            console.log(entries[0].isIntersecting)
-            let entry = entries[0];
-            if(entry.isIntersecting) this.player.play();
-            else this.player.pause();
-        } 
+    handlerIntersection(entries){
+        let entry = entries[0];
+        if(entry.isIntersecting) this.player.play();
+        else this.player.pause();
     }
 }
 
 export default AutoPause;
+
+// class AutoPause{
+//     run(player){
+//         this.player = player;
+//         const observer = new IntersectionObserver(this.handlerIntersection(), {
+//             threshold: 0.25
+//         })
+
+//         observer.observe(player.media);
+//     }
+
+//     handlerIntersection(){
+//         return (entries)=>{
+//             console.log(entries[0].isIntersecting)
+//             let entry = entries[0];
+//             if(entry.isIntersecting) this.player.play();
+//             else this.player.pause();
+//         } 
+//     }
+// }
+
+// export default AutoPause;
