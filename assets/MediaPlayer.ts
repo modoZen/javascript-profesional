b48aaa05e9e0b7e7e36d46d1010c09144c112795
Max <mdiazn@uni.pe>
2022-05-24 20:06:35 -0500

    clase40

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -6,16 +6,24 @@ interface ConfigParam{
 class MediaPlayer {
     media: HTMLMediaElement
     plugins:any[]
+    container:HTMLElement
+
     constructor(config:ConfigParam) {
         this.media = config.el;
         this.plugins = config.plugins || [];
-
+        this.initPlayer();
         this.initPlugins();
     }
     private initPlugins() {
         this.plugins.forEach(plugin => {
             plugin.run(this);
         });
+    }
+    initPlayer(){
+        this.container = document.createElement('div');
+        this.container.style.position = 'relative';
+        this.media.parentNode.insertBefore(this.container,this.media);
+        this.container.appendChild(this.media);
     }
     play() {
         this.media.play();
--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -6,14 +6,17 @@ export class AdsPlugin{
     private currentAd   :Ad
     private player:MediaPlayer
     private media :HTMLMediaElement
+    private adsContainer:HTMLElement
     constructor(){
         this.ads = Ads.getInstance();
         this.handlerTimeUpdate = this.handlerTimeUpdate.bind(this);
+        this.adsContainer = document.createElement('div')
     }
     run(player:MediaPlayer){
         this.player = player;
         this.media  = this.player.media;
         this.media.addEventListener('timeupdate',this.handlerTimeUpdate);
+        this.player.container.appendChild(this.adsContainer)
     }
     handlerTimeUpdate(){
         const currentTme = Math.floor(this.media.currentTime);
@@ -25,6 +28,19 @@ export class AdsPlugin{
         if(this.currentAd) return
         const ad = this.ads.getAd();
         this.currentAd = ad;
-        console.log(ad)
+        this.adsContainer.innerHTML = `
+        <div class="ads">
+            <a  class="ads__link" href="${this.currentAd.url}" target="_blank">
+                <img class="ads__img" src="${this.currentAd.imageUrl}" />
+                <div class="ads__info">
+                <h5 class="ads__title">${this.currentAd.title}</h5>
+                <p class="ads__body">${this.currentAd.body}</p>
+                </div>
+            </a>
+        </div>`;
+        setTimeout(()=>{
+            this.currentAd = null;
+            this.adsContainer.innerHTML ='';
+        },10000);
     }
 }
